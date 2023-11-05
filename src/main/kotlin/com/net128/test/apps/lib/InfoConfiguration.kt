c269150f6f93f3ccdc782244db07240c3d53da72
Michael Seger <michael@net128.com>
2023-11-05 19:55:18 +0100

    easy5

--- src/main/kotlin/com/net128/test/apps/lib/InfoConfiguration.kt
+++ src/main/kotlin/com/net128/test/apps/lib/InfoConfiguration.kt
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.PropertySource
 @Configuration
 @ConfigurationProperties(prefix = "info")
 @PropertySource(value = ["classpath:/com/net128/test/apps/lib/info.yaml"], factory = YamlPropertySourceFactory::class)
-class InfoConfiguration (
+data class InfoConfiguration (
     var attr1: String? = null,
-    var attr2: String? = null
+    var attr2: String? = null,
+    var attr3: String? = null,
+    var attr4: String? = "Value 4"
 )
